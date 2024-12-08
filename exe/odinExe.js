a39af26e332e40f227d00d68b0dd271285d41346
Sid2169 <siddharthasingh21109572@gmail.com>
2024-12-08 21:44:08 +0530

    Added solution to Exercise 1
    Exercise 1 :add <p> with red text that says “Hey I’m red!”

--- exe/odinExe.js
+++ exe/odinExe.js
@@ -3,3 +3,8 @@ const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text";
 container.appendChild(content);
+
+const red = document.createElement("p");
+red.textContent = "Hey I'm red!";
+red.style.color = "red";
+content.appendChild(red);
