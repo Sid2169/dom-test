3e7c3bef7847806998c52740b39eae81d88c3cad
Sid2169 <siddharthasingh21109572@gmail.com>
2024-12-08 21:50:58 +0530

    Addd solution to Exercise 2
    Exercise 2: Add an <h3> with blue text that says “I’m a blue h3!”

--- exe/odinExe.js
+++ exe/odinExe.js
@@ -8,3 +8,9 @@ const red = document.createElement("p");
 red.textContent = "Hey I'm red!";
 red.style.color = "red";
 content.appendChild(red);
+
+//Exercise 2 : Add an <h3> with blue text that says “I’m a blue h3!”
+const blueHeading = document.createElement("h3");
+blueHeading.textContent = "I'm a blue h3!";
+blueHeading.style.color = "blue";
+content.appendChild(blueHeading);
