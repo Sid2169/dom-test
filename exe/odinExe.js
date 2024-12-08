d56751d95d04c03a116ae78943059ef19143fd23
Sid2169 <siddharthasingh21109572@gmail.com>
2024-12-08 22:07:29 +0530

    Added solution  to exercise: 3 in TOP-DOM
    /Exercise 3 : Add a <div> with a black border and pink background color with the following elements inside of it:
    //another <h1> that says “I’m in a div”
    //a <p> that says “ME TOO!”

--- exe/odinExe.js
+++ exe/odinExe.js
@@ -14,3 +14,14 @@ const blueHeading = document.createElement("h3");
 blueHeading.textContent = "I'm a blue h3!";
 blueHeading.style.color = "blue";
 content.appendChild(blueHeading);
+
+//Exercise 3 : Add a <div> with a black border and pink background color with the following elements inside of it:
+//another <h1> that says “I’m in a div”
+//a <p> that says “ME TOO!”
+const pinkDiv = document.createElement("div");
+pinkDiv.innerHTML = `
+<h1>I'm in a div</h1>
+<p>ME TOO!
+`
+pinkDiv.style.cssText = "border: 1px solid black; background: pink;"
+content.appendChild(pinkDiv);
